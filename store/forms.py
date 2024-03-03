5788ba119b97bd2e0c0197d0b9022d275d13d7c6
AnastasiyaKokhan <nastya.kohan@inbox.ru>
2024-03-03 21:48:28 +0300

    fourth commit

--- store/forms.py
+++ store/forms.py
@@ -2,14 +2,15 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 from .models import Store, ProductColor, Product
-from .validators import validate_letters
+from .validators import validate_letters, validate_price
 
 
 class AddProductForm(forms.Form):
-    name = forms.CharField(max_length=300, label='Название*', validators=[validate_letters])
-    price = forms.DecimalField(max_digits=10, decimal_places=2, label='Цена*')
+    name = forms.CharField(max_length=300, validators=[validate_letters], label='Название*')
+    price = forms.DecimalField(max_digits=10, decimal_places=2, validators=[validate_price], label='Цена*')
     description = forms.CharField(initial='нет описания', widget=forms.Textarea, label='Описание')
-    colors = forms.ModelMultipleChoiceField(queryset=ProductColor.objects.all(), widget=forms.CheckboxSelectMultiple, label='Цвета*')
+    colors = forms.ModelMultipleChoiceField(queryset=ProductColor.objects.all(), widget=forms.CheckboxSelectMultiple,
+                                            label='Цвета*')
     image = forms.ImageField(initial='no_photo.png', label='Фотография')
     store = forms.ModelChoiceField(queryset=Store.objects.all(), widget=forms.RadioSelect, label='Магазин*')
 
@@ -28,10 +29,18 @@ class TestForm(forms.ModelForm):
         model = Product
         fields = ['name', 'price', 'description', 'colors', 'image', 'store']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'name'}),
-            'price': forms.NumberInput(attrs={'class': 'price'}),
-            'description': forms.Textarea(attrs={'class': 'description'}),
-            'colors': forms.CheckboxSelectMultiple(attrs={'class': 'colors'}),
+            'name': forms.TextInput(attrs={'class': 'input'}),
+            'price': forms.NumberInput(attrs={'class': 'input'}),
+            'description': forms.Textarea(attrs={'class': 'textarea'}),
+            'colors': forms.CheckboxSelectMultiple(attrs={'class': 'choice'}),
             'image': forms.FileInput(attrs={'class': 'image'}),
-            'store': forms.RadioSelect(attrs={'class': 'store'}),
+            'store': forms.RadioSelect(attrs={'class': 'choice'}),
+        }
+        labels = {
+            'name': 'Название',
+            'price': 'Цена',
+            'description': 'Описание',
+            'colors': 'Цвета',
+            'image': 'Фотография',
+            'store': 'Магазин',
         }
--- store/validators.py
+++ store/validators.py
@@ -3,4 +3,8 @@ from django.core.exceptions import ValidationError
 
 def validate_letters(word):
     if not word.isalpha():
-        raise ValidationError('можно вводить только буквы')
+        raise ValidationError('название не должно содержать цифры')
+
+def validate_price(number):
+    if not number > 0:
+        raise ValidationError('цена должна быть положительной')
